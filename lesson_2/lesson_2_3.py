2b80361d72d3b79c0f87278455b700e4ecbe24f8
romanmost321 <romanmostyko@icloud.com>
2026-04-03 15:46:37 +0300

    first

---
+++ lesson_2/lesson_2_3.py
@@ -0,0 +1,10 @@
+# Написать первую программу: программа должна выводить в консоль несколько строчек:
+# ● Hello, World
+# ● It is my first python program
+# ● And it is the end of my program
+# ● Bye-bye
+
+print ('Hello, World')
+print('It is my first python program')
+print('And it is the end of my program')
+print('Bye-bye')
